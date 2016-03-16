456695bc16bf698b11c5d21160c6a413457e80f4
moytho <moytho@hotmail.com>
2016-03-15 19:46:14 -0600

    nuevo commit, ignorando archivos innecesarios y archivos que contienen informacion sensible

---
+++ APITest/APITest/Models/MarcaDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace APITest.Models
+{
+    public class MarcaDTO
+    {
+        public int CodigoMarca { get; set; }
+        public int CodigoEmpresa { get; set; }
+        public string Nombre { get; set; }
+        public bool Estado { get; set; }
+    }
+}
